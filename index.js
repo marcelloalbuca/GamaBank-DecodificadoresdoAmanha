9bbc3615de2f6fda8dc187941601f5254e680de4
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2021-04-02 18:15:29 -0300

    Server

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log(`Execução em ambiente de ${process.env.NODE_ENV} http://localhost:8080/`  );
+
+
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 8080,
+    host: "localhost",
+  });
+
+  await server.start();
+};
+
+init();
+
+
+
