5e1bbd5d16193fb06cb7ab15b359c2b690d9e461
Matheus Vieira <mvieirinhaa@gmail.com>
2021-04-02 23:56:44 -0300

    estruturando projeto

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
-const server = require('./infra/server.js')
+const server = require('./infraestructure/server.js')
+
+const HOST = process.env.HOST || 'localhost'
+const PORT = process.env.PORT || 3000
 
 server.then(hapi => {
-	console.log(`server has been started on http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}`)
+	console.log(`server has been started on http://${HOST}:${PORT}`)
 	hapi.start()
 })
