50a047c1bfc7217a2b0eab972240f184be051a72
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2021-04-04 13:25:31 -0300

    tabela movimentacaoInterna e inserts

--- src/infraestructure/tabelasDB/movimentacaoInterna.sql
+++ src/infraestructure/tabelasDB/movimentacaoInterna.sql
@@ -10,3 +10,12 @@ FOREIGN KEY (idConta) REFERENCES contas(id),
 FOREIGN KEY (idUsuario) REFERENCES usuarios(id),
 FOREIGN KEY (idTransacao) REFERENCES transacoes(id)
 );
+
+INSERT INTO movimentacoesInterna (valor, idConta, idUsuario, idTransacao) values (600.00, 1 ,1, 1);
+INSERT INTO movimentacoesInterna (valor, idConta, idUsuario, idTransacao) values (100.00, 3 ,2, 2);
+INSERT INTO movimentacoesInterna (valor, idConta, idUsuario, idTransacao) values (200.00, 4 ,2, 3);
+INSERT INTO movimentacoesInterna (valor, idConta, idUsuario, idTransacao) values (60.00, 2 ,1, 1);
+INSERT INTO movimentacoesInterna (valor, idConta, idUsuario, idTransacao) values (10.00, 5 ,2, 2);
+INSERT INTO movimentacoesInterna (valor, idConta, idUsuario, idTransacao) values (20.00, 3 ,2, 3);
+
+select * from movimentacoesInterna;
