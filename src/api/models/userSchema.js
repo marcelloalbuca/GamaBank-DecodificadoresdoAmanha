d1bfbf7772a4ea72e6f2ba89c931b091549d66e2
Matheus Vieira <mvieirinhaa@gmail.com>
2021-04-07 21:44:44 -0300

    adicionando validação de campos

--- src/api/models/userSchema.js
+++ src/api/models/userSchema.js
@@ -1,8 +1,21 @@
 const Joi = require('@hapi/joi')
 
 module.exports = {
-  nome: Joi.string().min(3).required().label('Digite seu nome!'),
-  email: Joi.string().email().required(),
-  cpf: Joi.string().min(11).max(11).required(),
-  senha: Joi.string().min(6).required()
+  nome: Joi.string().
+    min(3).message('digite seu nome completo').
+    required().message('nome é requirido'),
+
+  email: Joi.string().
+    email().message('digite um e-mail válido').
+    required().message('email é requirido'),
+
+  cpf: Joi.string().
+    min(11).message('cpf precisa ter 11 numeros').
+    max(11).message('cpf precisa ter 11 numeros').
+    required().message('cpf é requirido'),
+
+  senha: Joi.string().
+    min(6).message('digite uma senha maior, minimo de caractere = 6').
+    required().message('senha é requirido')
+
 }
