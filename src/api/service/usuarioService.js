c56ae1f04d838e2fa8194030f7a1f6a69e753475
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2021-04-07 21:42:20 -0300

    adicionando try catch nos services

--- src/api/service/usuarioService.js
+++ src/api/service/usuarioService.js
@@ -2,25 +2,46 @@ const usuariosData = require('../data/usuariosData')
 const crypto = require('../../repositories/encryptPassword')
 
 exports.buscarUsuarios = async () => {
+  try{
   const rows = await usuariosData.buscarUsuarios()
   return rows
+  }catch (err){
+    throw err
+  }
 }
 
 exports.buscarUsuarioPorId = async (id) => {
-  const rows = usuariosData.buscarUsuarioPorId(id)
-  return rows
+
+  try{
+    const rows = await usuariosData.buscarUsuarioPorId(id)
+    return rows
+  }catch (err){
+    throw err
+  }
 }
 
 exports.criarUsuario = async (sqlStatement) => {
   const { senha } = sqlStatement
-  const encrypt = await crypto.encryptPassword(senha)
-  return usuariosData.criarUsuario(sqlStatement, encrypt)
+    try{
+      const encrypt = await crypto.encryptPassword(senha)
+      return usuariosData.criarUsuario(sqlStatement, encrypt)
+    }catch (err){
+      throw err
+    }
 }
 
 exports.deletarUsuarioPorId = async (sqlStatement) => {
+  try{
   return usuariosData.deletarUsuarioPorId(sqlStatement)
+  }catch (err){
+    throw err
+  }
 }
 
 exports.alterarUsuarioPorId = async (id, sqlStatement) => {
+  try{
   return usuariosData.alterarUsuarioPorId(id, sqlStatement)
+  }catch (err){
+    throw err
+  }
 }
