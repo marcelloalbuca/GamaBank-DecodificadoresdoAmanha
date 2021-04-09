6f2c784cec93976090b1a183096d164f4e44e89b
Marcello Albuquerque <marcello.albuca@gmail.com>
2021-04-09 14:38:31 -0300

    Update database.js

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql')
 
-/*const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'db_gamabank',
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'db_gamabank',
     port: 3306
-})*/
+})
 
 const execute = (sqlStatement) => {
     return new Promise((resolve, reject) => {
