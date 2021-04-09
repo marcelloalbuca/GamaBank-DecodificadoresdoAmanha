0308351168794585d402b625cf994872a4489fde
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2021-04-09 01:20:13 -0300

    user services

---
+++ src/api/services/user.service.js
@@ -0,0 +1,41 @@
+const repository = require('../repositories/user.repository')
+
+const createAccount = async (newUser) => {
+    // Opcional : verificar no banco de dados se usuario ja existe
+    const result = await repository.save(newUser)
+    return result
+}
+
+const buscarUsuarios = async () => {
+    const result = await repository.buscarUsuario()
+    return result
+}
+
+const buscarUsuarioPorId = async (id) => {
+    const result = await repository.buscarUsuarioPorId(id)
+    return result
+}
+
+const criarUsuario = async () => {
+    const result = await repository.criarUsuario()
+    return result
+}
+
+const deletarUsuarioPorId = async (id) => {
+    const result = await repository.deletarUsuarioPorId(id)
+    return result
+}
+
+const alterarUsuarioPorId = async (id) => {
+    const result = await repository.alterarUsuarioPorId()
+    return result
+}
+
+module.exports = { 
+    createAccount, 
+    buscarUsuarios,
+    buscarUsuarioPorId,
+    criarUsuario,
+    deletarUsuarioPorId,
+    alterarUsuarioPorId
+}
