45bc0bfecef3e57628d762f2e752489df501a994
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2021-04-09 01:22:31 -0300

    swagget config

---
+++ src/configs/swagger.js
@@ -0,0 +1,17 @@
+const inert = require("@hapi/inert");
+const vision = require("@hapi/vision");
+const hapiswagger = require('hapi-swagger')
+
+
+const swagger = {
+    plugin: hapiswagger,
+    options: {
+      info: {
+        title: 'Gamabank - Grupo Decodificadores do Amanhã',
+        description: 'Projeto desenvolvido durente o curso de Node.js - Accenture / Gama Academy',
+        version: '1.0'
+      }
+    }
+  }
+
+module.exports = [inert, vision, swagger]
