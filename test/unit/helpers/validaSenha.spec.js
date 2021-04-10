26c21564358b9b43d5788a0e4d0239a804345829
Matheus Vieira <mvieirinhaa@gmail.com>
2021-04-10 00:16:42 -0300

    reescrevendo testes com alterações das funções

--- test/unit/helpers/validaSenha.spec.js
+++ test/unit/helpers/validaSenha.spec.js
@@ -2,16 +2,11 @@ const { expect } = require('chai')
 
 const validaSenha = require('../../../src/helpers/validaSenha')
 
-// maximo de 13 letras
-// minimo 2 numeros
-// pelo menos 1 caractere especial
-// pelo menos uma letra Maiúscula
-
 describe("Função de validação de senhas especiais", () => {
   it('Deve retornar TRUE caso a senha atenda os requisitos',
     () => {
       const resultado = validaSenha("Panodeprado123@")
-      expect(resultado).to.be.equal(true)
+      expect(resultado).to.be.equal("Panodeprado123@")
     })
 
   it('Deve retornar FALSE se não digitar uma senha válida', () => {
