5582fa9b7f69262345e86d92e1caa42b0d9ff5ed
Matheus Vieira <mvieirinhaa@gmail.com>
2021-04-10 02:11:00 -0300

    refatorando função de comparação para uso na sessão de login

--- src/helpers/encryptPassword.js
+++ src/helpers/encryptPassword.js
@@ -3,13 +3,14 @@ const bcrypt = require('bcrypt')
 module.exports = {
   encryptPassword: async (password, salt) => {
     if (!salt) salt = await bcrypt.genSalt()
+
     return {
       encryptedPassword: bcrypt.hashSync(password, salt),
       salt: salt
     }
   },
-  comparePassword: async (password, salt, encryptedPasswordToCompare) => {
-    const encryptedPassword = bcrypt.hashSync(password, salt)
-    return encryptedPassword === encryptedPasswordToCompare
+
+  comparePassword: async (password, encryptedPasswordToCompare) => {
+    return await bcrypt.compare(password, encryptedPasswordToCompare)
   }
 }
