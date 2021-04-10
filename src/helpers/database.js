fc0655d327085452de8d5b1f7a8c6a148db55d61
Matheus Vieira <mvieirinhaa@gmail.com>
2021-04-10 00:19:15 -0300

    organizando codigo da instancia do banco de dados

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -3,15 +3,15 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'db_gamabank',
+    password: process.env.DB_PASSWORD || '12345',
+    database: process.env.DB_NAME || 'gamabank',
     port: 3306
 })
 
 const execute = (sqlStatement) => {
     return new Promise((resolve, reject) => {
         connection.query(sqlStatement, (err, result) => {
-            if(err) reject(err)
+            if (err) reject(err)
             else resolve(result)
 
             //connection.end()
