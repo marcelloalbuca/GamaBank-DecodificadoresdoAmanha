dbd80e8818529c9164e1509b234eeba2e060488a
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2021-04-12 12:44:23 -0300

    corrigindo query extrato

--- src/api/repositories/extrato.repository.js
+++ src/api/repositories/extrato.repository.js
@@ -3,22 +3,19 @@ const { execute } = require('../../helpers/database')
 
 const listarExtratoPorId = async (id, h) => {
     try {
-        const sqlStatement = `select u.id, u.nome as usario_nome, t.nome as transacao_nome, c.valor, c.data_criacao as data, c.idConta, cn.saldo from  usuarios u
+        const sqlStatement = `select u.id, u.nome as usario_nome, t.nome as transacao_nome, c.valor, c.data_criacao as data from  usuarios u
         inner join compras c on c.idUsuario = u.id 
         inner join transacoes t on c.idTransacao = t.id
-        inner join contas cn on c.idConta = cn.id
         WHERE u.id = ${id}
         union 
-        select u.id, u.nome as usario_nome, t.nome as transacao_nome, mi.valor, mi.data_criacao as data, mi.idConta, cn.saldo from usuarios u
+        select u.id, u.nome as usario_nome, t.nome as transacao_nome, mi.valor, mi.data_criacao as data from usuarios u
         inner join movimentacoesInterna mi on mi.idUsuario = u.id 
         inner join transacoes t on mi.idTransacao = t.id
-        inner join contas cn on mi.idConta = cn.id
         WHERE u.id = ${id}
         union
-        select u.id, u.nome as usario_nome, t.nome as transacao_nome, me.valor, me.data_criacao as data, me.idConta, cn.saldo from usuarios u
+        select u.id, u.nome as usario_nome, t.nome as transacao_nome, me.valor, me.data_criacao as data from usuarios u
         inner join movimentacoesExterna me on me.idUsuario = u.id 
         inner join transacoes t on me.idTransacao = t.id
-        inner join contas cn on me.idConta = cn.id
         WHERE u.id = ${id}
         order by data DESC;`
 
